e93d76efedc1cd1c3889d9e78d7a03b9d784353c
ishani <91791452+deathmukh@users.noreply.github.com>
2023-07-27 19:00:15 +0530

    initial commit

---
+++ robospeaker/main.py
@@ -0,0 +1,13 @@
+import pyttsx3
+
+if __name__=="__main__" :
+    print("Welcome to RoboSpeaker , created by Deathmukh ")
+    engine = pyttsx3.init()
+    while True :
+        enter = input("What do you want to hear from me today? ")
+        if enter == "exit" :
+            engine.say("Ok bye")
+            engine.runAndWait()
+            break
+        engine.say({enter})
+        engine.runAndWait()    
